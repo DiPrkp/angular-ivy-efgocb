1485ac8696a1b7693244cf2f771e5d863993c9fe
DiPrkp <di.prokopenko@mail.ru>
2021-12-13 14:26:05 +0300

    2 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,6 +10,26 @@ import { products } from '../products';
 export class ProductListComponent {
   products = products;
 
+  fontColor = 'red';
+  sayHelloId = 2;
+  canClick = false;
+ 
+  message = 'I\'m read only!';
+  canEdit = false;
+
+  sayMessage() {
+    alert(this.message);
+  }
+
+  onEditClick() {
+    this.canEdit = !this.canEdit;
+    if (this.canEdit) {
+      this.message = 'You can edit me!';
+    } else {
+      this.message = 'I\'m read only!';
+    }
+  }
+
   share() {
     window.alert('The product has been shared!');
   }
