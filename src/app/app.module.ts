f051f3f65fb4c61683bb24da8f37a32aad96a6d1
DiPrkp <di.prokopenko@mail.ru>
2021-12-14 15:30:15 +0300

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,15 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import {ProductDetailsComponent} from './product-details/product-details.component';
+import { CartService } from "./cart.service"
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
-      { path: '', component: ProductListComponent},
-      { path: 'products/:productId', component: ProductDetailsComponent},
+      { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent },
     ])
   ],
   declarations: [
@@ -23,10 +24,13 @@ import {ProductDetailsComponent} from './product-details/product-details.compone
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
   ],
   bootstrap: [
     AppComponent
+  ],
+  providers: [
+    CartService
   ]
 })
 export class AppModule { }
